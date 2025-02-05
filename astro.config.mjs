35188e1f26d121ff752a7a27a7020b64892649b5
miphazhang <miphazhang@tencent.com>
2025-02-05 20:47:12 +0800

    init personal page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
+import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx()],
+});
